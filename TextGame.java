2a0eac89aa4cc7d15a55ee1a380284b8816a7a73
Łukasz <lukasz.krupa87@gmail.com>
2017-12-28 15:15:11 +0100

    poprawki
    Poprawilem komentarze, wprowadzilem metode combat w calym programie;

--- TextGame.java
+++ TextGame.java
@@ -12,26 +12,29 @@ import java.util.Scanner;
  */
 public class TextGame {
     
-    public int Combat(int player, int beast){
+    //algorytm walki pomiedzy graczem i potworem bez modyfikatorow; walka
+    public int Combat(int playerHP, int beastHP){
         //Random rand = new Random();
-            while(beast>0 || player>0){
-                beast-= (int) Math.random() * 6;
-                beast -= (int) (Math.random() * 5);
-                if(beast <= 0){
+            while(beastHP>0 || playerHP>0){
+                
+                //atakuje gracz
+                beastHP -= (int) (Math.random() * 8 +2);
+                if(beastHP <= 0){
                     System.out.println("Bestia ma 0 HP. Zabiles potwora..");
                     break;
                 }
-                player -= (int) (Math.random() * 5);
-                if(player <= 0){
+                
+                //atakuje potwor
+                playerHP -= (int) (Math.random() * 5);
+                if(playerHP <= 0){
                     System.out.println("Masz 0 HP. Zginales..");
                     break;
                 }
             }
-            //int x=0;
-            //x= (int) Math.random() * 6;
-            return beast;
+            return playerHP;
         }
 
+    
     public static void main(String[] args) {
         /**
          * @param playerHP Player life
@@ -40,25 +43,26 @@ public class TextGame {
 
         //zmienne odpowiadajace za ilosc zycia zawodnika i przeciwnikow;
         int playerHP = 50, monsterHP, injuries;
-        String playerName = "", question = "1", choice = "0";
+        String playerName = "", question = "1";
         boolean state = true;
-
+        
         //Random rand = new Random();
         Scanner sc = new Scanner(System.in);
-
+        TextGame gra = new TextGame();
+        
         System.out.println("Witaj obiezyswiecie w mojej grze!\n Znalezlismy Cie ledwo przytomnego.");
 
         //glowny program
         while (state) {
             switch (question) {
-                //pytanie 1.
+                //pytanie 1. -> 2.
                 case "1":
                     System.out.println("1) Powiedz nam jak sie nazywasz?");
                     playerName = sc.next();
                     question = "2";
                     break;
 
-                //pytanie 2.
+                //pytanie 2. -> pytanie 3.|1.;
                 case "2":
                     System.out.println("\n2) Czy Twoje imie to " + playerName + "?\n 1| tak..\n 2| nie..\n");
                     switch (sc.next()) {
@@ -74,18 +78,20 @@ public class TextGame {
                     }
                     break;
 
-                //pytanie 3.
+                //pytanie 3. -> pytanie 4.|5.
                 case "3":
                     System.out.println("\n3) Witaj " + playerName + "! Jestes w naszym bunkrze. Na zewnatrz jest pelno stworow.. Co robisz?");
                     System.out.println("1| Ide i walcze!\n2| O co w tym wszystkim chodzi?\n3| zostaje i sie nie ruszam..\n");
                     switch (sc.next()) {
                         case "1":
+                            
                             System.out.println("\nWylatujesz z pelna para na jednego z goblinow..");
                             monsterHP = (int) Math.random() * 10 +1; //(obrazenia od 1 do 10);
-                            injuries = monsterHP - (int) Math.random() * 3 +1; //obrazenia od 1 do 4;
-                            playerHP = playerHP - injuries;
+                            gra.Combat(playerHP, monsterHP);
+                            //injuries = monsterHP - (int) Math.random() * 3 +1; //obrazenia od 1 do 4;
+                            //playerHP = playerHP - injuries;
                             System.out.println("Po heroicznej walce z przywodca goblinow masz teraz " + playerHP + " zycia");
-                            System.out.println("Odniosles " + injuries + " obrazen");
+                            //System.out.println("Odniosles " + injuries + " obrazen");
                             question = "4";
                             break;
                         case "2":
@@ -94,17 +100,19 @@ public class TextGame {
                         case "3":
                             System.out.println("\nWalczysz z goblinami, ktore wtargnely do bunkra!");
                             monsterHP = (int) Math.random() * 10 +1; //(obrazenia od 1 do 10);
-                            injuries = monsterHP - (int) Math.random() * 10 +1;
-                            playerHP = playerHP - injuries;
-                            System.out.println("\nOtrzymales: " + injuries + " obrazen. Pozostalo Ci: " + playerHP + " zycia.");
+                            gra.Combat(playerHP, monsterHP);
+                            //injuries = monsterHP - (int) Math.random() * 10 +1;
+                            //playerHP = playerHP - injuries;
+                            System.out.println("po meznej walce mosz teroz " + playerHP + " zycia");
+                            //System.out.println("\nOtrzymales: " + injuries + " obrazen. Pozostalo Ci: " + playerHP + " zycia.");
                             question = "4";
                             break;
                         default:
-                            System.out.println("\nPodaj liczbe 1, 2 lub 3.");
+                            System.out.println("\nNie wciskaj mi glupot.. Podaj liczbe 1, 2 lub 3.");
                     }
                     break;
 
-                //pytanie 4.
+                //pytanie 4. -> 5.
                 case "4":
                     System.out.println("\n4) Przypadkowo natrafiles na miecz jednego z goblinow, ale uslyszales dziwny szelest. Co robisz?");
                     System.out.println("1| Podnosze miecz.\n2| Chowam sie za pobliskim murkiem i czekam.\n3| Pytam sie kto to.\n");
@@ -123,7 +131,7 @@ public class TextGame {
                             question = "5";
                             break;
                         case "3":
-                            System.out.println("\nZ krzakow wychodzi maly kroliczek, ktory z przerazona minka ucieka od Ciebie.");
+                            System.out.println("\nZ za krzakow wychodzi maly kroliczek, ktory z przerazona minka ucieka od Ciebie.");
                             question = "5";
                             break;
                         default:
@@ -131,41 +139,45 @@ public class TextGame {
                     }
                     break;
 
-                //pytanie 5.
+                //pytanie 5. -> end.
                 case "5":
                     System.out.println("\n5) Ludzie podchodza do Ciebie blizej. Jestesmy ocalalymi po najezdzie tych dziwnych stworow. Potrzebujemy meznych wojow do walki z nimi takich jak Ty..");
-                    System.out.println("1| Walcze!!\n2| chowasz sie\n");
+                    System.out.println("1| Walcze!!\n2| Chowam sie\n");
                     switch (sc.next()) {
                         case "1":
                             System.out.println("\nRzucasz mieczem w glowe goblina zombie.");
                             monsterHP = (int) Math.random() * 10 +1;
-                            injuries = monsterHP + (int) Math.random() * 35 +1; //dodatkowy modyfikator obrazen;
+                            gra.Combat(playerHP, monsterHP);
+                            /*injuries = monsterHP + (int) Math.random() * 35 +1; //dodatkowy modyfikator obrazen;
                             if (injuries > playerHP) {
                                 System.out.println("\nNiestety minales jego glowe! przegrales..");
                                 state = false;
                             } else {
                                 System.out.println("\nMiecz trafil do celu. Zabiles przywodce.. zabijajac go reszta pieszchla. Wygrales");
                                 state = false;
-                            }
+                            }*/
+                            state= false;
                             break;
                         case "2":
                             System.out.println("\nCala horda goblinow zombie wparowuje i zabija wszystkich po kolei w koncu trafiajac na Ciebie");
                             monsterHP = (int) Math.random() * 10 +1;
-                            injuries = monsterHP + (int) Math.random() * 10 +1; //dodatkowy modyfikator obrazen;
+                            gra.Combat(playerHP, monsterHP);
+                            /*injuries = monsterHP + (int) Math.random() * 10 +1; //dodatkowy modyfikator obrazen;
                             if (injuries > playerHP) {
                                 System.out.println("\nNiestety podczas walki umarles..\nNastepnym razem wiecej odwagi!");
                                 state = false;
                             } else {
                                 System.out.println("\nJakims cudem pokonales gobliny! Dziekujemy za dzielna walke");
                                 state = false;
-                            }
+                            }*/
+                            state= false;
                             break;
                         default:
                             System.out.println("\nPodaj liczbe 1, 2 lub 3.");
                     }
                     break;
 
-                //Jesli nie ma takiego nru pytanie lub wybrano co innego.
+                //Jesli nie ma takiego numeru pytanie lub wybrano co innego.
                 default:
                     System.out.println("\nNie ma takiego pytania");
                     System.out.println("Czy chcesz zakonczyc?\n 1|tak\n 2|nie");
