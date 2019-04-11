583f47472d103660efb863518e7442298c39cd48
Łukasz <lukasz.krupa87@gmail.com>
2019-04-11 16:59:15 +0200

    Add files via upload

--- TextGame.java
+++ TextGame.java
@@ -1,16 +1,19 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Ten program nalezy do Lukasza Krupy.
+ * Mozna kopiowac kod zrodlowy ale nie odpowiadam za jego stan.
+ * Jesli macie propozycje co do zmian albo propozycje pracy to prosze pisac :)
+ * lukasz.krupa87@gmail.com
  */
 package textgame;
 
 import java.util.Scanner;
 
 /**
- * @author lkrupa
+ * @author mozig
  */
 public class TextGame {
+    
+    
     
     //algorytm walki pomiedzy graczem i potworem bez modyfikatorow; walka
     public int Combat(int playerHP, int beastHP){
@@ -18,7 +21,7 @@ public class TextGame {
             while(beastHP>0 || playerHP>0){
                 
                 //atakuje gracz
-                beastHP -= (int) (Math.random() * 8 +2);
+                beastHP -= (int) (Math.random() * 5 +3);  //3 to modyfikator ataku gracza
                 if(beastHP <= 0){
                     System.out.println("Bestia ma 0 HP. Zabiles potwora..");
                     break;
@@ -31,9 +34,8 @@ public class TextGame {
                     break;
                 }
             }
-            return playerHP;
-        }
-
+        return playerHP;
+    }
     
     public static void main(String[] args) {
         /**
@@ -41,9 +43,12 @@ public class TextGame {
          * @param monsterHP Monster life
          */
 
-        //zmienne odpowiadajace za ilosc zycia zawodnika i przeciwnikow;
-        int playerHP = 50, monsterHP, injuries;
-        String playerName = "", question = "1";
+        //int playerHP = 50, monsterHP, injuries;
+        //Inicjalizacja objektow- postaci w grze;
+        Player bohater= new Player();
+        Beast goblin= new Beast();
+        
+        String question = "1";
         boolean state = true;
         
         //Random rand = new Random();
@@ -58,13 +63,13 @@ public class TextGame {
                 //pytanie 1. -> 2.
                 case "1":
                     System.out.println("1) Powiedz nam jak sie nazywasz?");
-                    playerName = sc.next();
+                    bohater.setNazwa(sc.next());
                     question = "2";
                     break;
 
                 //pytanie 2. -> pytanie 3.|1.;
                 case "2":
-                    System.out.println("\n2) Czy Twoje imie to " + playerName + "?\n 1| tak..\n 2| nie..\n");
+                    System.out.println("\n2) Czy Twoje imie to " + bohater.getNazwa() + "?\n 1| tak..\n 2| nie..\n");
                     switch (sc.next()) {
                         case "1":
                             question = "3";
@@ -80,17 +85,16 @@ public class TextGame {
 
                 //pytanie 3. -> pytanie 4.|5.
                 case "3":
-                    System.out.println("\n3) Witaj " + playerName + "! Jestes w naszym bunkrze. Na zewnatrz jest pelno stworow.. Co robisz?");
+                    System.out.println("\n3) Witaj " + bohater.getNazwa() + "! Jestes w naszym bunkrze. Na zewnatrz jest pelno stworow.. Co robisz?");
                     System.out.println("1| Ide i walcze!\n2| O co w tym wszystkim chodzi?\n3| zostaje i sie nie ruszam..\n");
                     switch (sc.next()) {
                         case "1":
-                            
                             System.out.println("\nWylatujesz z pelna para na jednego z goblinow..");
-                            monsterHP = (int) Math.random() * 10 +1; //(obrazenia od 1 do 10);
-                            gra.Combat(playerHP, monsterHP);
+                            goblin.setHP((int) Math.random() * 40); //(obrazenia od 1 do 10);
+                            gra.Combat(bohater.getHP(), goblin.getHP());
                             //injuries = monsterHP - (int) Math.random() * 3 +1; //obrazenia od 1 do 4;
                             //playerHP = playerHP - injuries;
-                            System.out.println("Po heroicznej walce z przywodca goblinow masz teraz " + playerHP + " zycia");
+                            System.out.println("Po heroicznej walce z przywodca goblinow masz teraz " + bohater.getHP() + " zycia");
                             //System.out.println("Odniosles " + injuries + " obrazen");
                             question = "4";
                             break;
@@ -99,11 +103,11 @@ public class TextGame {
                             break;
                         case "3":
                             System.out.println("\nWalczysz z goblinami, ktore wtargnely do bunkra!");
-                            monsterHP = (int) Math.random() * 10 +1; //(obrazenia od 1 do 10);
-                            gra.Combat(playerHP, monsterHP);
+                            goblin.setHP((int) Math.random() * 40); //(obrazenia od 1 do 10);
+                            gra.Combat(bohater.getHP(), goblin.getHP());
                             //injuries = monsterHP - (int) Math.random() * 10 +1;
                             //playerHP = playerHP - injuries;
-                            System.out.println("po meznej walce mosz teroz " + playerHP + " zycia");
+                            System.out.println("po meznej walce mosz teroz " + bohater.getHP() + " zycia");
                             //System.out.println("\nOtrzymales: " + injuries + " obrazen. Pozostalo Ci: " + playerHP + " zycia.");
                             question = "4";
                             break;
@@ -119,15 +123,15 @@ public class TextGame {
                     switch (sc.next()) {
                         case "1":
                             System.out.println("\nPodnoszac miecz poczules przyplyw mocy.");
-                            playerHP = playerHP + 10;
-                            System.out.println("Otrzymales dodatkowo 10pktow zdrowia. Teraz masz: " + playerHP + "pktow zycia");
+                            bohater.setHP(bohater.getHP()+10);
+                            System.out.println("Otrzymales dodatkowo 10pktow zdrowia. Teraz masz: " + bohater.getHP() + "pktow zycia");
                             System.out.println("Przy okazji zobaczyles ze to byl tylko maly przestraszony krolik, ktory sobie pokical dalej.");
                             question = "5";
                             break;
                         case "2":
                             System.out.println("To tylko maly kroliczek... ktory rzucil Ci sie do gardla bo myslal ze sie na Niego czaisz... ;)");
                             System.out.println("Tracisz 10pktow zycia");
-                            playerHP = playerHP - 10;
+                            bohater.setHP(bohater.getHP()-10);
                             question = "5";
                             break;
                         case "3":
@@ -146,8 +150,8 @@ public class TextGame {
                     switch (sc.next()) {
                         case "1":
                             System.out.println("\nRzucasz mieczem w glowe goblina zombie.");
-                            monsterHP = (int) Math.random() * 10 +1;
-                            gra.Combat(playerHP, monsterHP);
+                            goblin.setHP((int) Math.random() * 40);
+                            gra.Combat(bohater.getHP(), goblin.getHP());
                             /*injuries = monsterHP + (int) Math.random() * 35 +1; //dodatkowy modyfikator obrazen;
                             if (injuries > playerHP) {
                                 System.out.println("\nNiestety minales jego glowe! przegrales..");
@@ -160,8 +164,8 @@ public class TextGame {
                             break;
                         case "2":
                             System.out.println("\nCala horda goblinow zombie wparowuje i zabija wszystkich po kolei w koncu trafiajac na Ciebie");
-                            monsterHP = (int) Math.random() * 10 +1;
-                            gra.Combat(playerHP, monsterHP);
+                            goblin.setHP((int) Math.random() * 40);
+                            gra.Combat(bohater.getHP(), goblin.getHP());
                             /*injuries = monsterHP + (int) Math.random() * 10 +1; //dodatkowy modyfikator obrazen;
                             if (injuries > playerHP) {
                                 System.out.println("\nNiestety podczas walki umarles..\nNastepnym razem wiecej odwagi!");
